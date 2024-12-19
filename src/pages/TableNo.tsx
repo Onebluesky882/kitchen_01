3b9ff872057e0019cae504f1643f957c07743ae3
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-12-19 21:08:40 +0700

    reset

--- src/pages/TableNo.tsx
+++ src/pages/TableNo.tsx
@@ -9,31 +9,12 @@ import OrderTableCard, {
 const TableNo = () => {
   const { tableNo = "" } = useParams<{ tableNo?: string }>();
 
-  const { order, table } = useContext(GlobalContext).orderProvider;
-
-  const selectedTable = table.find((no) => no.tableNo === tableNo);
-
-  const orderTable = order.filter(
-    (item) => item.tableNo === selectedTable?.tableNo
-  );
-  console.log("orderTable", orderTable);
   return (
     <div>
       <h1 style={{ textAlign: "center", color: "blueviolet" }}>
         <TableNoCard tableNo={tableNo} />
       </h1>
-      <OrderTableContainer>
-        {orderTable.map((menu) => (
-          <OrderTableCard
-            tableNo={menu.tableNo}
-            amount={menu.amount}
-            name={menu.name}
-            price={menu.price}
-            image={menu.image}
-            key={menu.id}
-          />
-        ))}
-      </OrderTableContainer>
+      <OrderTableContainer></OrderTableContainer>
     </div>
   );
 };
